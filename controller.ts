96957df854033cee515732d72804ded474043f2f
Gabriel Puehler <gabrielgmp22@gmail.com>
2020-06-07 18:05:46 -0300

    Update controller.ts
    Incluído todas as formatações.

--- controller.ts
+++ controller.ts
@@ -1,16 +1,30 @@
 import * as tipos from "https://deno.land/x/case/mod.ts";
 
-
 const getLink = ({response} :{response:any}) => {
     response.body = {'Teste' : tipos.camelCase('testando teste')}
 };
 
 const postLink = async ({request,response} : {request:any,response:any}) => {
-    const palavra = await request.body(JSON);
+    const result  = await request.body(JSON);
+    const palavra = result.value.key_wrong;
 
     response.body = { 'Formatações' : {
-        'Camel Case': tipos.camelCase(palavra.value.key_wrong),
-        
+        'Camel Case': tipos.camelCase(palavra),
+        'Constant Case': tipos.constantCase (palavra),
+        'Dot Case': tipos.dotCase(palavra),
+        'Headers Case': tipos.headerCase (palavra),
+        'Lower Case': tipos.lowerCase (palavra),
+        'Lower First Case': tipos.lowerFirstCase(palavra),
+        'Normal Case': tipos.normalCase  (palavra),
+        'Param Case': tipos.paramCase (palavra),
+        'Pascal Case': tipos.pascalCase (palavra),
+        'Path Case': tipos.pathCase (palavra),
+        'Sentence Case': tipos.sentenceCase (palavra),
+        'Snake Case': tipos.snakeCase (palavra),
+        'Swap Case': tipos.swapCase (palavra),
+        'Title Case': tipos.titleCase (palavra),
+        'Upper Case': tipos.upperCase (palavra),
+        'Upper First Case': tipos.upperFirstCase (palavra),
     }};
 };
 
